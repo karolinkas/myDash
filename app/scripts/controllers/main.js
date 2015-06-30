78398922a31d68c099b096c8cae547fd2699c31e
Karolinka <li-nee@gmx.de>
2015-06-30 20:11:35 +0200

    added logfile to load

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('myDashApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope','SimpleHttpLoader', function ($scope,SimpleHttpLoader) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.log ={
+    	src: 'files/access.log',
+    	data: ''
+    }; 
+
+    new SimpleHttpLoader($scope.log.src)
+     .then(function(response){
+      $scope.log.data = response.data;
+     });
+  }]);
 

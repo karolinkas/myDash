3df50cc1657a0305d3e5a3763723fa9dfab64ef1
Karolinka <li-nee@gmx.de>
2015-07-01 10:10:07 +0200

    using JSONP callback

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,19 @@
 'use strict';
 
 angular.module('myDashApp')
-  .controller('MainCtrl', ['$scope','SimpleHttpLoader', function ($scope,SimpleHttpLoader) {
+  .controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $scope.log ={
-    	src: 'files/access.log',
-    	data: ''
-    }; 
+    
+	    var url = 'https://data.sparkfun.com/output/dZ4EVmE8yGCRGx5XRX1W?callback=JSON_CALLBACK';
+	    $http.jsonp(url)
+	    .success(function(data){
+	        console.log(data.found);
+	    });
 
-    new SimpleHttpLoader($scope.log.src)
-     .then(function(response){
-      $scope.log.data = response.data;
-     });
   }]);
 
